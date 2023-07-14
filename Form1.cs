a1fd139c7086180af1df701892bf34d240cc479e
NatanaelDp <135743045+NatanaelDp@users.noreply.github.com>
2023-07-14 17:14:16 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04AppBanco
+{
+    public partial class Form1 : Form
+    {
+        List<string> extratos = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            double valor = double.Parse(txtValor.Text);
+            double saldo = double.Parse(lblSaldo.Text.Replace("R$", ""));
+            double soma;
+            if (lblOperacao.Text=="SAQUE")
+            {
+                 soma =  saldo - valor;
+                extratos.Add("Saque no valor de R$ " + valor);
+
+            }
+            else
+            {
+                 soma = valor + saldo;
+                extratos.Add("Depósito no valor de R$ " + valor);
+            }
+           
+            lblSaldo.Text = "R$" + soma;
+            
+
+        }
+
+        private void lblSaldo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            //tira a cor do botão de depositar
+            btnDepositar.BackColor = Color.Gainsboro;
+            btnDepositar.ForeColor = Color.Black; 
+            //Pinta o botão de sacar
+            btnSacar.BackColor = Color.LightBlue;
+            btnSacar.ForeColor = Color.DodgerBlue;
+            lblOperacao.Text = "SAQUE";
+
+        }
+
+        private void btnDepositar_Click(object sender, EventArgs e)
+        {
+            btnSacar.BackColor = Color.Gainsboro;
+            btnSacar.ForeColor = Color.Black;
+            btnDepositar.BackColor = Color.LightBlue;
+            btnDepositar.ForeColor = Color.DodgerBlue;
+            lblOperacao.Text = "DEPÓSITO";
+        }
+
+        private void btnExtrato_Click(object sender, EventArgs e)
+        {
+            Form2 telaExtrato = new Form2();
+            telaExtrato.extratos = extratos;
+            telaExtrato.Show();
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula04AppBanco
+{
+    public partial class Form2 : Form
+    {
+        //Variáveis globais
+        public List<string> extratos = new List<string>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int contador = 0;
+
+
+            for(int i=0;i<extratos.Count;i++)
+            {
+                listExtratos.Items.Add(extratos[i]);
+            }
+        }
+    }
+}
